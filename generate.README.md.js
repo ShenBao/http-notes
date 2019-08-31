e050b302f6a46839b12d9fdf3aed27fc93367660
ShenBao <shenbaoone@gmail.com>
2019-08-31 21:54:36 +0800

    feat: add README.md

---
+++ generate.README.md.js
@@ -0,0 +1,105 @@
+var fileName = 'README.md';
+var repName = 'http-notes';
+var basePath = `https://github.com/ShenBao/${repName}/blob/master`;
+basePath = '';
+
+var desc = `
+HTTP 学习笔记
+`;
+
+var endDesc = `
+## More links
+
+- [GitHub Home](https://github.com/ShenBao)
+- [Blog Home](https://shenbao.github.io)
+- [About Me](https://shenbao.github.io/about/)
+`;
+
+var fs = require ('fs');
+var path = require ('path');
+
+function readFileList (dir, filesList = []) {
+  const files = fs.readdirSync (dir);
+  files.forEach ((item, index) => {
+    var fullPath = path.join (dir, item);
+    const stat = fs.statSync (fullPath);
+    if (stat.isDirectory ()) {
+      var nextPath = path.join (dir, item);
+      if (
+        nextPath.includes ('code') ||
+        nextPath.includes ('img') ||
+        nextPath.includes ('.git')
+      ) {
+      } else {
+        filesList.push (
+          {},
+          {
+            name: item,
+            path: fullPath.replace (__dirname, '').replace (/\\/g, '/') +
+              '/README.md',
+          }
+        );
+        readFileList (path.join (dir, item), filesList);
+      }
+    } else {
+      var extname = path.extname (item);
+      if (extname == '.md' && item != 'README.md') {
+        var name = item.replace ('.md', '');
+        var arr = name.split ('.');
+        if (/^[0-9]+[\s\S]*$/.test (item)) {
+          arr.shift ();
+        }
+        name = arr.join ().replace (/(^\s*)|(\s*$)/g, '');
+        var itemPath = fullPath.replace (__dirname, '').replace (/\\/g, '/');
+        var obj = {
+          name: name,
+          path: itemPath,
+        };
+        filesList.push (obj);
+      }
+    }
+  });
+  return filesList;
+}
+
+var filesList = [];
+readFileList (__dirname, filesList);
+
+var str = '';
+filesList.forEach ((item, index) => {
+  var {name, path} = item;
+  if (!name) {
+    str += `\n`;
+    return;
+  }
+  var enPath = encodeURIComponent (path);
+  if (item.path.includes ('README.md')) {
+    console.log (``);
+    str += `## [${name}](${basePath}${enPath})\n\n`;
+  } else {
+    str += `1. [${name}](${basePath}${enPath})\n`;
+  }
+  console.log (`[${name}](${basePath}${path})`);
+});
+
+if (!basePath.startsWith ('http')) {
+  str += endDesc;
+}
+
+var content = `# ${repName}\n`;
+if (!basePath.startsWith ('http')) {
+  content += desc;
+}
+fs.writeFileSync (`./${fileName}`, content);
+fs.writeFile (
+  `./${fileName}`,
+  str,
+  {flag: 'a', encoding: 'utf-8', mode: '0666'},
+  function (err) {
+    if (err) {
+      console.log ('\n======== error ========');
+      throw err;
+    }
+    console.log ('\n======== success ========');
+  }
+);
---
+++ md-2：大话 HTTP 协议/README.md
@@ -0,0 +1,61 @@
+# 大话 HTTP 协议
+
+- 介绍
+
+- HTTP 初相识 - 了解 HTTP 协议
+    - 浏览器背后的故事
+    - HTTP 协议的前世今生
+    - 透过 TCPIP 看 HTTP
+    - "你是如何访问 www.xxx.com 的" - DNS 域名解析试看
+    - 回溯 HTTP 事务处理过程
+    - 实验：与 HTTP 请求的第一次亲密接触
+
+- HTTP 再邂逅 - 熟悉 HTTP 协议结构和通讯原理
+    - HTTP 协议特点
+    - 详解 URL 、URI 与 URN 的区别与联系
+    - HTTP 报文结构分析
+    - HTTP 请求方法剖析
+    - HTTP 响应状态码拆解
+    - 实验：用 telnet 分析 http 协议的通讯过程
+    - HTTP 状态管理：Cookie 与 Session
+
+- HTTP 渐相知 - 深入认识 HTTP 协议不为人知的特性和使用方法
+    - 聊聊 HTTP 协议中的编码和解码
+    - HTTP 协议之基本认证
+    - HTTP 中长连接与短连接
+    - HTTP 中介之代理
+    - HTTP 中介之网关
+    - HTTP 缓存
+    - HTTP 内容协商机制
+    - HTTP 的断点续传与多线程下载
+
+- HTTP 紧相伴 - 实验使用 HTTP 协议构建应用
+    - HTTP 请求服务器的简单实现
+    - HTTP 服务的搭建部署
+    - HTTP 抓包分析实践
+    - HTTP 请求客户端实验
+
+- HTTP 静相依 - 安全的 HTTPS
+    - 趣解 HTTP 与 HTTPS
+    - HTTPS 协议概述
+    - HTTPS 使用成本
+    - HTTPS 对性能的影响
+    - HTTPS 常见问题
+    - 实验：从 HTTP 到 HTTPS - 设计属于自己的安全请求服务
+
+- HTTP 长相随 - 基于 HTTP 的功能追加协议
+    - HTTP 协议的瓶颈
+    - 双工通信的 WebScocket
+    - 探索式的实践 - SPDY
+    - 期盼已久的 HTTP2.0
+    - 实验：HTTP1.1 升级 HTTP2.0 那些事
+    - 管理 WEB 服务器文件的 WebDAV 协议
+    - HTTP 的大跃进 - QUIC 与 HTTP3.0
+
+- HTTP 终相守 - Web 安全威胁解析
+    - Web 安全攻击概述
+    - 验证机制安全
+    - 会话管理机制
+    - SQL 注入攻击
+    - 跨站脚本攻击
+    - CSRF 攻击
